f8656fc8fca2b811efe840f31c7bdd4397c422a7
LeoPlanche <43221407+LeoPlanche@users.noreply.github.com>
2023-05-03 17:21:29 +0300

    init

---
+++ HMM.py
@@ -0,0 +1,392 @@
+import msprime
+import tskit
+import random
+import numpy as np
+from copy import deepcopy
+
+
+random.seed()  
+
+
+states = (0,1,2)
+
+def initS(a) -> np.array:
+    S = np.zeros(3)
+    S[0]=1-2*a
+    S[1]=a
+    S[2]=a
+    return S
+    
+def initA(Ti,Tn,r,L,a) -> np.array:
+    A = np.zeros((3,3))
+    A[0][1]=Ti*r*L*a
+    A[0][2]=Tn*r*L*a
+    A[1][0]=Ti*r*L*(1-2*a)
+    A[1][2]=Tn*r*L*a
+    A[2][0]=Tn*r*L*(1-2*a)
+    A[2][1]=Tn*r*L*a
+    A[0][0]=1-A[0][1]-A[0][2]
+    A[1][1]=1-A[1][0]-A[1][2]
+    A[2][2]=1-A[2][0]-A[2][1]
+    return A
+
+#Tn : Split time between the two Nd families
+#Tea:  Split time between Asia and Europe
+def initB(m,L,Ti,Ta,Tea,Tn) -> np.array: 
+    print("Start Init B")
+    B = np.empty(shape=(3,30,30))
+    meani = m*L*Ti
+    meana = m*L*Ta
+    meanea = m*L*Tea
+    meann = m*L*Tn
+    Pea = np.empty(30)
+    Pi_ea = np.empty(30)
+    Pa_ea = np.empty(30)
+    Pn=np.empty(30)
+    Pa_n=np.empty(30)
+    Pea[0]=np.exp(-meanea)
+    Pi_ea[0]=np.exp(-(meani-meanea))
+    Pa_ea[0]=np.exp(-(meana-meanea))
+    Pn[0]=np.exp(-(meann))
+    Pa_n[0]=np.exp(-(meana-meann))
+    sumea=0
+    sumi_ea=0
+    suma_ea=0
+    sumn=0
+    suma_n=0
+    for i in range(1,30):
+        Pea[i]=Pea[i-1]*meanea/i
+        Pi_ea[i]=Pi_ea[i-1]*(meani-meanea)/i
+        Pa_ea[i]=Pa_ea[i-1]*(meana-meanea)/i
+        Pn[i]=Pn[i-1]*meann/i
+        Pa_n[i]=Pa_n[i-1]*(meana-meann)/i
+        sumea=sumea+Pea[i]
+        sumi_ea=sumi_ea+Pi_ea[i]
+        suma_ea=suma_ea+Pa_ea[i]
+        sumn=sumn+Pn[i]
+        suma_n=suma_n+Pa_n[i]
+
+    Pea[0]=1-sumea
+    Pi_ea[0]=1-sumi_ea
+    Pa_ea[0]=1-suma_ea
+    Pn[0]=1-sumn
+    Pa_n[0]=1-suma_n
+  
+    for i in range(30):
+        for j in range(30):
+            if j<=i:
+                B[0][i][j]=Pea[j]*Pi_ea[i-j]
+                B[1][i][j]=Pea[j]*Pa_ea[i-j]
+                B[2][i][j]=Pn[j]*Pa_n[i-j]
+            else:
+                B[0][i][j]=0
+                B[1][i][j]=0
+                B[2][i][j]=0
+    return B
+
+#Ti : Out of Africa
+#Ta:  Split time between Nd and Sp
+#Tea: Split time between Eu and As
+#Tn: Split time between the two Nd families
+def initBNotCond(m,L,Ti,Ta,Tea,Tn) -> np.array:
+    print("Start Init B")
+    B = np.empty(shape=(3,30,30))
+    meani = m*L*Ti
+    meana = m*L*Ta
+    meanea = m*L*Tea
+    meann = m*L*Tn
+    Pea = np.empty(30)
+    Pa=np.empty(30)
+    Pi=np.empty(30)
+
+    Pn=np.empty(30)
+    Pea[0]=np.exp(-meanea)
+    Pn[0]=np.exp(-(meann))
+    Pi[0]=np.exp(-(meani))
+    Pa[0]=np.exp(-(meana))
+    suma=0
+    sumea=0
+    sumi=0
+    sumn=0
+    for i in range(1,30):
+        Pea[i]=Pea[i-1]*meanea/i
+        Pn[i]=Pn[i-1]*meann/i
+        Pi[i]=Pi[i-1]*meani/i
+        Pa[i]=Pa[i-1]*meana/i
+        sumea=sumea+Pea[i]
+        sumn=sumn+Pn[i]
+        sumi=sumi+Pi[i]
+        suma=suma+Pa[i]
+
+    Pea[0]=1-sumea
+    Pn[0]=1-sumn
+    Pi[0]=1-sumi
+    Pa[0]=1-suma
+
+  
+    for i in range(30):
+        for j in range(30):
+            B[0][i][j]=Pi[i]*Pea[j] #Not Nd
+            B[1][i][j]=Pa[i]*Pea[j] #Nd in Asia
+            B[2][i][j]=Pa[i]*Pn[j]  #Nd not in Asia
+  
+    return B
+
+
+def print_dptable(V):
+    print("      ",)
+    for i in range(len(V)): print("%8d" % i,)
+    print()
+
+    for y in V[0].keys():
+        print("%7.7s: " % y,)
+        for t in range(len(V)):
+            print("%.8s" % ("%f" % V[t][y]),)
+        print()
+
+
+            
+def viterbi(V, initial_distribution, a, b):
+    T = len(V)
+    M = a.shape[0]
+ 
+    omega = np.zeros((T, M))
+    omega[0, :] = np.log(initial_distribution * b[:, V[0][0],V[0][1]])
+ 
+    prev = np.zeros((T - 1, M))
+ 
+    for t in range(1, T):
+        for j in range(M):
+            # Same as Forward Probability
+            probability = omega[t - 1] + np.log(a[:, j]) + np.log(b[j, V[t][0], V[t][1]])
+ 
+            # This is our most probable state given previous state at time t (1)
+            prev[t - 1, j] = np.argmax(probability)
+ 
+            # This is the probability of the most probable state (2)
+            omega[t, j] = np.max(probability)
+ 
+    # Path Array
+    S = np.zeros(T)
+ 
+    # Find the most probable last hidden state
+    last_state = np.argmax(omega[T - 1, :])
+ 
+    S[0] = last_state
+ 
+    backtrack_index = 1
+    for i in range(T - 2, -1, -1):
+        S[backtrack_index] = prev[i, int(last_state)]
+        last_state = prev[i, int(last_state)]
+        backtrack_index += 1
+ 
+    # Flip the path array since we were backtracking
+    S = np.flip(S, axis=0)
+ 
+    # Convert numeric values to actual hidden states
+ 
+    result = []
+    for s in S:
+        if s == 0:
+            result.append(0)
+        elif s == 1:
+            result.append(1)
+        elif s == 2:
+            result.append(2)
+ 
+    return result
+
+
+def posterior(sequence,start,transitions,emissions):
+    res = initialize_matrix(len(states),len(sequence))
+    f=forwardlog(sequence,start,transitions,emissions)
+    b=backwardlog(sequence,start,transitions,emissions)
+    for i in range(0,len(states)):
+        for j in range(0,len(sequence)):
+            res[i][j]=np.exp(f[i][j+1]+b[i][j+1]-f[-1][-1])
+    return res
+            
+
+def forwardlog(sequence,start,transitions,emissions):
+    F = initialize_matrix(len(states),len(sequence)+2)
+    F[0][0] = 1
+    for i in range(0,len(states)):
+        F[i][1] = np.log(start[states[i]])+np.log(emissions[states[i]][sequence[0]])
+    for j in range(2,len(sequence)+1):
+        for i in range(0,len(states)):
+            p_sum = 0
+            sA = []
+            for k in range(0,len(states)):
+                if  transitions[(states[k],states[i])]*emissions[states[i]][sequence[j-1]]==0:
+                     p_sum +=0
+                else:
+                    sA.append(F[k][j-1]+np.log(transitions[(states[k],states[i])])+np.log(emissions[states[i]][sequence[j-1]]))
+            for  q in range(0,len(sA)):     
+                p_sum += np.exp(sA[q]-max(sA))                      
+            F[i][j] = np.log(p_sum)+max(sA)
+    p_sum = 0
+    sA = []
+    for k in range(0,len(states)):
+        sA.append(F[k][len(sequence)])
+    for  q in range(0,len(sA)):     
+        p_sum += np.exp(sA[q]-max(sA))  
+    F[-1][-1] = np.log(p_sum)+max(sA)
+    return F
+
+def backwardlog(sequence,start,transitions,emissions):
+    F = initialize_matrix(len(states),len(sequence)+1)
+    for i in range(0,len(states)):
+        F[i][-1] = 0
+    for j in range(len(sequence)-1,0,-1): 
+        for i in range(0,len(states)):
+            p_sum = 0
+            sA=[]
+            for k in range(0,len(states)):
+                if transitions[(states[i],states[k])]*emissions[states[k]][sequence[j]] ==0:
+                    p_sum=0
+                else:
+                    sA.append(F[k][j+1]+np.log(transitions[(states[i],states[k])])+np.log(emissions[states[k]][sequence[j]]))
+            for  q in range(0,len(sA)):     
+                p_sum += np.exp(sA[q]-max(sA))                      
+            F[i][j] = np.log(p_sum)+max(sA)      
+    p_sum = 0
+    sA=[]
+    for k in range(0,len(states)):
+        if start[states[k]]*emissions[states[k]][sequence[0]] ==0:
+            p_sum +=0
+        else:
+            sA.append(F[k][1]+np.log(start[states[k]]*emissions[states[k]][sequence[0]]))
+    for  q in range(0,len(sA)):     
+         p_sum += np.exp(sA[q]-max(sA)) 
+    F[0][0] = np.log(p_sum)+max(sA)
+    return F
+
+
+
+
+def training( obs_seq,S,A,B, maxIter=100000) -> (np.array,np.array,np.array):
+
+        N = len(S)
+        M = 30
+        T = len(obs_seq)
+        print("Ajustement du HMM par rapport Ã  %d observations" % T)
+        iters = 0
+        oldLogProb = float("-inf")
+        logProb = 0.0
+        while iters < maxIter:
+            iters=iters+1
+            print("ItÃ©ration %d, Ã©tat du HMM:" % iters)
+            #self.impress()
+            #the alpha-pass
+            alpha = []
+            c=[]
+            #init
+            c.append(0)
+            alpha.append([])
+            for i in range(N):
+                alpha[0].append(S[i]*B[i,obs_seq[0][0],obs_seq[0][1]])
+                c[0]=c[0]+alpha[0][i]
+            c[0]=1.0/c[0]
+            for i in range(N):
+                alpha[0][i]=c[0]*alpha[0][i]
+            
+            #compute
+            for t in range(1,T):
+                c.append(0.0)
+                alpha.append([])
+                for i in range(N):
+                    alpha[t].append(0.0)
+                    for j in range(N):
+                        alpha[t][i]=alpha[t][i]+alpha[t-1][j]*A[i][j]
+                    alpha[t][i]=alpha[t][i]*B[i,obs_seq[t][0],obs_seq[t][1]]
+                    c[t]=c[t]+alpha[t][i]
+                c[t]=1.0/c[t]
+                for i in range(N):
+                    alpha[t][i]=c[t]*alpha[t][i]
+
+            #Compute log[P(Obs_seq)]
+            logProb=0.0
+            for i in range(T):
+                logProb=logProb+np.log(c[i])
+            logProb=-logProb
+            print("Log[P(ObsSeq)] = %f\n" % logProb)
+            if logProb > oldLogProb:
+                oldLogProb=logProb
+            else:
+                break
+
+            #beta-pass
+            beta = []
+            beta.append([])
+            for i in range(N):
+                beta[0].append(c[T-1])
+            for t in range(T-2,-1,-1):
+                beta.insert(0,[])
+                for i in range(N):
+                    beta[0].append(0.0)
+                    for j in range(N):
+                        beta[0][i]=beta[0][i]+A[i][j]*B[j,obs_seq[t+1][0],obs_seq[t+1][1]]*beta[1][j]
+                    beta[0][i]=c[t]*beta[0][i]
+
+            #compute gama-ti and gama-tij
+            gamaTi = []
+            gamaTij = []
+            for t in range(T-1):
+                denom=0.0
+                for i in range(N):
+                    for j in range(N):
+                        denom=denom+alpha[t][i]*A[i][j]*B[j,obs_seq[t+1][0],obs_seq[t+1][1]]*beta[t+1][j]
+                gamaTi.append([])
+                gamaTij.append([])
+                for i in range(N):
+                    gamaTi[t].append(0.0)
+                    gamaTij[t].append([])
+                    for j in range(N):
+                        gamaTij[t][i].append((alpha[t][i]*A[i][j]*B[j,obs_seq[t+1][0],obs_seq[t+1][1]]*beta[t+1][j])/denom)
+                        gamaTi[t][i] = gamaTi[t][i] + gamaTij[t][i][j]
+
+            #Save start, trans, emission probabilities
+            oldStart_p = deepcopy(S)
+            oldTrans_p = deepcopy(A)
+            oldEmission_p = deepcopy(B)
+
+
+            #Re-estimate start, transition and emission probabilities
+            #Start
+            for i in range(N):
+                S[i]=gamaTi[0][i]
+
+            #Transition
+            for i in range(N):
+                for j in range(N):
+                    numer = 0.0
+                    denom = 0.0
+                    for t in range(T-1):
+                        numer = numer+gamaTij[t][i][j]
+                        denom = denom + gamaTi[t][i]
+                    A[i][j]=numer/denom
+
+            #Emission
+            for i in range(N):
+                for j in range(M):
+                    for k in range(M):
+                        numer = 0.0
+                        denom = 0.0
+                        for t in range(T-1):
+                            if obs_seq[t][0] == j and obs_seq[t][1] == k:
+                                numer = numer+gamaTi[t][i]
+                            denom=denom+gamaTi[t][i]
+                        B[i,j,k]=numer/denom
+
+        S = oldStart_p
+        A = oldTrans_p
+        B = oldEmission_p
+        return (S,A,B)
+    
+def initialize_matrix(dim1,dim2,value=0):
+    F = []
+    for i in range(0,dim1):
+        F.append([])
+        for j in range(0,dim2):
+            F[i].append(value)
+    return F
---
+++ core.py
@@ -0,0 +1,327 @@
+import msprime
+import tskit
+import random
+import time
+
+import numpy as np
+
+
+
+random.seed()  
+
+
+        
+
+#Get all tracts that migrated in pop.
+#Run faster than get_migrating_tracts_ind, but does not distinguish between individuals
+
+def get_migrating_tracts(ts,pop):
+    neanderthal_id = [p.id for p in ts.populations() if p.metadata['name']==pop][0]
+    migrating_tracts = []
+    for migration in ts.migrations():
+        if migration.dest == neanderthal_id:
+            migrating_tracts.append((migration.left, migration.right))
+    return np.array(migrating_tracts)
+
+
+# Input:
+#      pop: a population
+#      ind: id of the node corresponding to the individual
+# Output: 
+#      all tracts from ind that migrated in pop
+        
+def get_migrating_tracts_ind(ts,pop,ind,size=1000):
+    start_time = time.time()
+    pop_id = [p.id for p in ts.populations() if p.metadata['name']==pop][0]
+    mig = ts.tables.migrations
+    migrating_tracts = []
+    for tree in ts.trees():
+        u = ind
+        flag=True
+        while u != tskit.NULL and flag:
+            migs = np.where(mig.node == u)[0]
+            for cur_mig in migs:
+                if flag:
+                    cur_mig = mig[cur_mig]
+                    if(cur_mig.dest==pop_id and cur_mig.left<=tree.interval.left and cur_mig.right>=tree.interval.right):
+                        flag=False
+                        if(len(migrating_tracts)>0 and tree.interval.left==migrating_tracts[len(migrating_tracts)-1][1]):
+                            migrating_tracts[len(migrating_tracts)-1][1]=tree.interval.right                     
+                        else:
+                            migrating_tracts.append([tree.interval.left,tree.interval.right])
+
+            u = tree.parent(u)
+    #Clean duplicates
+    print(time.time()-start_time)
+    start_time = time.time()
+    tracts = migrating_tracts
+    flag = True
+    while(flag):
+        i=0
+        flag=False
+        while i < len(tracts) and not flag:
+            l=[]
+            j=i+1
+            while j < len(tracts):
+                if tracts[i][0]==tracts[j][0] and tracts[i][1]==tracts[j][1]:
+                    l.append(j)
+                    flag=True
+                j=j+1
+            cnt=0
+            for j in l:
+                tracts.pop(j-cnt)
+                cnt=cnt+1
+            i=i+1
+    print(time.time()-start_time)
+    migrating_tracts = clean_tracts(migrating_tracts,size)
+    return migrating_tracts
+
+
+
+def createSeqObs(ts,cut,ind,p1,p2,p3) -> list:
+    print("Start createSeqObs")
+    tables = ts.dump_tables()
+    nodes = tables.nodes
+    seq = np.zeros((int(ts.sequence_length/cut),2),dtype=int) #List of seq, a seq is a list with nb of mutations
+    african_id = [p.id for p in ts.populations() if p.metadata['name']=="Af"][0]
+    asian_id = [p.id for p in ts.populations() if p.metadata['name']=="As2"][0]
+    
+    start_time = time.time()
+    pop=[]
+    r=0
+    for v in ts.variants():
+        i=int(v.site.position/cut)
+        
+        r=r+1
+        b = False
+        c = False
+        d = False
+        j=0
+        x=0
+        while x < len(v.genotypes):
+            if(nodes[x].individual==ind):
+                if(v.genotypes[x]==1):
+                    b = True
+                elif not b and x==p1-1:
+                    x=p1+p2+p3
+            elif(b and nodes[x].population==african_id):
+                if(v.genotypes[x]==1):
+                    c = True
+                    x=p1+p2-1
+                    
+            elif(b and nodes[x].population==asian_id):
+                if(v.genotypes[x]==1):
+                    d = True 
+                    x=p1+p2+p3-1
+            j=j+1
+            x=x+1
+        if b and not c :
+            seq[i][0]=seq[i][0]+1
+        if b and not c and not d :
+            seq[i][1]=seq[i][1]+1
+    print("End createSeqObs")
+
+    return seq
+
+# Input:
+#   ts:  A Tree Sequence
+#   cut: The length of the tracts
+#   ind: An individual id
+#   pop: A population name
+#Output:
+#   For each tract, the number of mutation in ind that is not found in pop
+
+
+def createSeqObsSingle(ts,cut,ind,pop,max_sample=1000) -> list:
+    tables = ts.dump_tables()
+    nodes = tables.nodes
+    seq = np.zeros(int(ts.sequence_length/cut),dtype=int)  #The list which will contain the result of our function.
+    pop_id = [p.id for p in ts.populations() if p.metadata['name']==pop][0] 
+    start=-1
+    end=-1
+    for i in range(len(nodes)):
+        if nodes[i].population==pop_id and start ==-1:
+            start = i
+        if nodes[i].population!=pop_id and start !=-1:
+            end = i-1
+            break
+    if end-start > max_sample:
+        end = start+max_sample-1
+
+    for v in ts.variants():
+        i=int(v.site.position/cut)
+        c = False
+        x=0
+        while x <= end: 
+            if(nodes[x].individual==ind): # Check is this position corresponds to individual 'ind'
+                b=v.genotypes[x]
+                x=start
+            elif(nodes[x].population==pop_id): # Check is this position corresponds to an individual in 'pop'
+                if(v.genotypes[x]==b): 
+                    c = True # The mutation found in 'ind' is also present in the population 'pop' 
+                    x = end # We can skip all the remaining individuals
+            x=x+1
+            
+            
+        if not c : # If c is False it means that the mutation in 'ind' has not been found in the population 'pop' 
+            seq[i]=seq[i]+1
+    return seq
+
+
+# Returns the list of tracts from the output of the HMM
+def get_HMM_tracts(seq):
+    migrating_tracts = []
+    maxi = 0
+    for e in seq:
+        if e>maxi:
+            maxi=e
+            
+    for i in range(maxi+1):
+        migrating_tracts.append([])
+    start=0
+    for i in range(1,len(seq)):
+        if seq[i]!=seq[i-1]:
+            migrating_tracts[seq[i-1]].append([start,i-1])
+            start=i
+    migrating_tracts[seq[len(seq)-1]].append([start,len(seq)-1])
+    return migrating_tracts
+
+# Remove duplicates, merge adjacent tracts, and sort them.
+def clean_tracts(tractInit,size=1000):
+    tract = np.copy(tractInit)
+    tract = tract/size
+    tract=tract.astype(int)
+    flag = True
+    while(flag):
+        flag=False
+        for i in range(len(tract)):
+            for j in range(len(tract)):
+                if not flag and tract[i,0]==tract[j,1]:
+                    tract[j,1]=tract[i,1]
+                    tract = np.delete(tract,i,0)
+                    flag=True
+    flag = True
+    while(flag):
+        flag=False
+        for i in range(len(tract)):
+            for j in range(i+1,len(tract)):
+                if tract[i,0]>tract[j,0]:
+                    save0=tract[i,0]
+                    save1=tract[i,1]
+                    tract[i,0]=tract[j,0]
+                    tract[i,1]=tract[j,1]
+                    tract[j,0]=save0
+                    tract[j,1]=save1
+                    flag=True
+    return tract
+
+
+def inTracts(pos,tract):
+    for i in range(len(tract)):
+        if pos>=tract[i][0] and pos <= tract[i][1]:
+            return True
+    return False
+            
+
+def get_tract_length(tracts):
+    l=0
+    for t in tracts:
+        l=l+t[1]-t[0]+1
+    return l
+
+
+#Return the tracts that are in both input
+def intersect_tracts(tracts1,tracts2):
+    max = 0
+    res =[]
+    for t in tracts1:
+        if t[1]>max:
+            max=t[1]
+    for t in tracts2:
+        if t[1]>max:
+            max=t[1]
+    b1=False
+    b2=False
+    inside=False
+    start=-1
+    for i in range(max+1):
+        if (inTracts(i,tracts1)):
+            b1=True
+        else:
+            b1=False
+        if (inTracts(i,tracts2)):
+            b2=True
+        else:
+            b2=False
+        if (b1 and b2 and not inside):
+            inside = True
+            start = i
+        elif( not(b1 and b2) and inside):
+            inside = False
+            res.append([start,i])
+    return res
+
+#Return the first tract minus the second
+def substract_tracts(tracts1,tracts2):
+    max = 0
+    res =[]
+    for t in tracts1:
+        if t[1]>max:
+            max=t[1]
+    for t in tracts2:
+        if t[1]>max:
+            max=t[1]
+    b1=False
+    b2=True
+    inside=False
+    start=-1
+    for i in range(max+1):
+        if (inTracts(i,tracts1)):
+            b1=True
+        else:
+            b1=False
+        if (inTracts(i,tracts2)):
+            b2=False
+        else:
+            b2=True
+        if (b1 and b2 and not inside):
+            inside = True
+            start = i
+        elif( not(b1 and b2) and inside):
+            inside = False
+            res.append([start,i])
+    return res
+        
+
+def detected_tracts(tracts1,tracts2):
+    tractsC1 = np.copy(tracts1)
+    tractsC2 = np.copy(tracts2)
+    isDetected = np.zeros(len(tractsC2))
+    count=0
+    acc=0
+    for i in range(len(tractsC1)):
+        j=0
+        flag=True
+        while flag and j < len(tractsC2):
+            if tractsC1[i,0]<tractsC2[j,1] and tractsC1[i,1]>tractsC2[j,0]:
+                diff = abs(tractsC1[i,0]-tractsC2[j,0])+abs(tractsC1[i,1]-tractsC2[j,1])
+                common = min(tractsC1[i,1],tractsC2[j,1])-max(tractsC1[i,0],tractsC2[j,0])
+                if common>4*diff:
+                    flag=False
+                    count=count+1
+                    acc=acc+(common/(common+diff))
+                    isDetected[j]=1
+                    
+            j=j+1
+    acc=acc/count
+    lenD=0
+    lenND=0
+    for i in range(len(tractsC2)):
+        if isDetected[i]==1:
+            lenD=lenD+tractsC2[i,1]-tractsC2[i,0]
+        else:
+            lenND=lenND+tractsC2[i,1]-tractsC2[i,0] 
+        
+    return(count,acc,len(tractsC1)-count,len(tractsC2)-count)
+
+
